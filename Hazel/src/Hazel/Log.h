190078c6cf1e47e555cbb2d87a564d0f011e2e45
Roger Leon Borras <36153923+rleonborras@users.noreply.github.com>
2019-09-04 13:54:28 +0200

    Added and understood precomiled Headers

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -3,8 +3,6 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
 
-#include <memory>
-
 
 //Core log Macros
 
---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif // HZ_PLATFORM_WINDOWS
